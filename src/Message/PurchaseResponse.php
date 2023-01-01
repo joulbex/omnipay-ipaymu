3f3aad7838c7f58930feda371c3c9fd36e857fd7
Aleksander Meloch <aleksander.meloch@gmail.com>
2023-01-01 22:37:38 +0100

    Added missing methods in PurchaseResponse object.

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -3,17 +3,56 @@
 	namespace Omnipay\iPaymu\Message;
 
 	use Omnipay\Common\Message\AbstractResponse;
+	use Omnipay\Common\Message\RedirectResponseInterface;
 
-	class PurchaseResponse extends AbstractResponse
+	class PurchaseResponse extends AbstractResponse implements RedirectResponseInterface
 	{
 		/**
 	     * When you do a `purchase` the request is never successful because
 	     * you need to redirect off-site to complete the purchase.
+	     * TODO: Is this correct?
 	     *
 	     * {@inheritdoc}
 	     */
 	    public function isSuccessful()
 	    {
 	        return false;
+	    }
+
+	    public function isRedirect()
+	    {
+	        return isset($this->data['Data']['Url']);
+	    }
+
+	    /**
+	     * Gets the redirect target url.
+	     *
+	     * @return string
+	     */
+	    public function getRedirectUrl()
+	    {
+	    	if ($this->isRedirect()) {
+	            return $this->data['Data']['Url'];
+	        }
+	    }
+
+	    /**
+	     * Get the required redirect method (either GET or POST).
+	     *
+	     * @return string
+	     */
+	    public function getRedirectMethod()
+	    {
+	    	return 'GET';
+	    }
+
+	    /**
+	     * Gets the redirect form data array, if the redirect method is POST.
+	     *
+	     * @return array
+	     */
+	    public function getRedirectData()
+	    {
+	    	return [];
 	    }
 	}
