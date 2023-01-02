609c98644f5dc1d9aabecc88843c1aedc6ce2e9e
Aleksander Meloch <aleksander.meloch@gmail.com>
2023-01-02 16:06:44 +0100

    PHPunit setup.

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -1,1 +1,25 @@
 <?php
+
+	namespace Omnipay\iPaymu;
+
+	use Omnipay\Tests\GatewayTestCase;
+
+	class GatewayTest extends GatewayTestCase
+	{
+		protected $gateway;
+
+	    public function setUp(): void
+	    {
+	        parent::setUp();
+
+	        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+	    }
+
+	    public function testPurchase()
+	    {
+	        $request = $this->gateway->purchase(array('amount' => '10.00'));
+
+	        $this->assertInstanceOf('Omnipay\iPaymu\Message\PurchaseRequest', $request);
+	        $this->assertSame('10.00', $request->getAmount());
+	    }
+	}
